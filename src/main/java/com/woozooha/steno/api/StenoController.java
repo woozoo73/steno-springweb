a65c25c9fd981fccb93f7fac04eaacb22d3242bc
woozoo73 <woozoo73@gmail.com>
2022-07-03 14:08:19 +0900

    polish

--- src/main/java/com/woozooha/steno/api/StenoController.java
+++ src/main/java/com/woozooha/steno/api/StenoController.java
@@ -1,62 +1,32 @@
 package com.woozooha.steno.api;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.woozooha.steno.conf.Config;
 import com.woozooha.steno.model.Story;
-import lombok.SneakyThrows;
+import com.woozooha.steno.model.StoryExample;
+import com.woozooha.steno.writer.Reader;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.nio.charset.StandardCharsets;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.List;
 
 @RestController
 @RequestMapping("/steno-/stories/")
 public class StenoController {
 
-    private static final ObjectMapper objectMapper = new ObjectMapper();
-
     @GetMapping
-    public List<Story> getStories() {
-        Story story = new Story();
-        File dayDir = story.getDateDir();
-        if (!dayDir.exists()) {
-            return Collections.emptyList();
-        }
+    public List<Story> getStories(StoryExample example) {
+        Reader reader = Config.getCurrent().getReader();
 
-        File[] files = dayDir.listFiles();
-        Arrays.sort(files, Comparator.comparingLong(File::lastModified).reversed());
-
-        return Arrays.stream(files).map(f -> {
-                            try {
-                                return readStory(f);
-                            } catch (Exception e) {
-                                return null;
-                            }
-                        }
-                )
-                .filter(Objects::nonNull)
-                .collect(Collectors.toList());
+        return reader.getStoryList(example);
     }
 
     @GetMapping("/{id}")
     public Story getStory(@PathVariable String id) {
-        List<Story> stories = this.getStories();
+        Reader reader = Config.getCurrent().getReader();
 
-        return stories.stream().filter(s -> id.equals(s.getId())).findFirst().orElse(null);
-    }
-
-    @SneakyThrows
-    private Story readStory(File file) {
-        try (Reader reader = new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8)) {
-            return objectMapper.readValue(reader, Story.class);
-        }
+        return reader.getStory(id);
     }
 
 }
